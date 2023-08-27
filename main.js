1c8c59367788f08e693a0bcf14cb3e6d17deaa3a
Devashish <devashishdeshpande04@gmail.com>
2023-08-27 22:24:22 +0530

    minor changes

--- main.js
+++ main.js
@@ -14,11 +14,17 @@ const scene = new THREE.Scene();
 const bloomScene = new THREE.Scene();
 
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+const camera1 = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#bg") });
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
+
+const bloomRenderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#bg") });
+bloomRenderer.setPixelRatio(window.devicePixelRatio)
+bloomRenderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(bloomRenderer.domElement);
 
 const geometry = new THREE.TorusGeometry(10, 1, 10, 100);
 
@@ -30,7 +36,7 @@ scene.add(cube);
 
 
 
-function generateStars(num, stars, scene) {
+function generateStars(stars, scene1) {
 
   for(let count = 0 ; count<stars ; count++){
     const [x,y,z]= Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
@@ -41,12 +47,12 @@ function generateStars(num, stars, scene) {
     });
     const star = new THREE.Mesh(sphere,material);
     star.position.set(x, y, z);
-    scene.add(star);
+    scene1.add(star);
   }
 }
 
 
-generateStars(100, 250, bloomScene);
+generateStars( 250, bloomScene);
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5)
 scene.add(pointLight)
@@ -70,14 +76,23 @@ bloomPass.radius = 1;
 // const outputPass = new OutputPass();
 //const bloomer = new BloomEffect()
 
-const composer = new EffectComposer( renderer );
+const composer = new EffectComposer( bloomRenderer );
 composer.addPass( renderPass );
 composer.addPass( bloomPass );
 // composer.addPass( outputPass );
 
 const controls = new OrbitControls(camera, renderer.domElement);
-renderer.autoClear = false;
 
+renderer.autoClear = false;
+bloomRenderer.autoClear = false;
+
+// bloomScene.renderOrder(0,true);
+// scene.renderOrder(0,true);
+camera.aspect =  window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
+
+camera1.aspect =  window.innerWidth / window.innerHeight;
+camera1.updateProjectionMatrix();
 
 function animate() {
   rotation()
@@ -86,8 +101,9 @@ function animate() {
   controls.update();
   composer.render();
 
-  renderer.render(bloomScene, camera);
+  bloomRenderer.render(bloomScene, camera);
   renderer.render(scene, camera);
+
 }
 function rotation() {
   cube.rotation.x += 0.004;
