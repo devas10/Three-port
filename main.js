418b3cbdc3cc2711f1a1c046f488602bd6d59265
Devashish <devashishdeshpande04@gmail.com>
2023-08-27 20:36:28 +0530

    two-scenes-attempt-fail

--- main.js
+++ main.js
@@ -11,14 +11,14 @@ import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
 import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
 
 const scene = new THREE.Scene();
+const bloomScene = new THREE.Scene();
+
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
 const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#bg") });
 renderer.setPixelRatio(window.devicePixelRatio)
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-
 
 const geometry = new THREE.TorusGeometry(10, 1, 10, 100);
 
@@ -28,7 +28,9 @@ const material = new THREE.MeshStandardMaterial({
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-function generateStars(num,stars, scene) {
+
+
+function generateStars(num, stars, scene) {
 
   for(let count = 0 ; count<stars ; count++){
     const [x,y,z]= Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
@@ -44,13 +46,13 @@ function generateStars(num,stars, scene) {
 }
 
 
-generateStars(100,250, scene);
+generateStars(100, 250, bloomScene);
 const pointLight = new THREE.PointLight(0xffffff);
 pointLight.position.set(5, 5, 5)
 scene.add(pointLight)
 
 const ambientLight = new THREE.AmbientLight(0xffffff, 10000)
-//scene.add(ambientLight)
+bloomScene.add(ambientLight)
 
 const lighthelper = new THREE.PointLightHelper(pointLight)
 const gridHelper = new THREE.GridHelper(200, 50)
@@ -58,12 +60,12 @@ scene.add(lighthelper, gridHelper)
 
 camera.position.setZ(30);
 
-const renderPass = new RenderPass( scene, camera );
+const renderPass = new RenderPass( bloomScene, camera );
 
 const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
-bloomPass.threshold = 0;
-bloomPass.strength = 1;
-bloomPass.radius = 0;
+bloomPass.threshold =0.4;
+bloomPass.strength = 2;
+bloomPass.radius = 1;
 
 // const outputPass = new OutputPass();
 //const bloomer = new BloomEffect()
@@ -73,14 +75,19 @@ composer.addPass( renderPass );
 composer.addPass( bloomPass );
 // composer.addPass( outputPass );
 
-const controls = new OrbitControls(camera, renderer.domElement)
+const controls = new OrbitControls(camera, renderer.domElement);
+renderer.autoClear = false;
+
 
 function animate() {
   rotation()
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-  controls.update()
+
+  controls.update();
   composer.render();
+
+  renderer.render(bloomScene, camera);
+  renderer.render(scene, camera);
 }
 function rotation() {
   cube.rotation.x += 0.004;
