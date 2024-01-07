f1d8a8905d454df31cb40986ce81429d644c24cf
Devashish <devashishdeshpande04@gmail.com>
2024-01-08 00:43:59 +0530

    click-Bloom

--- main.js
+++ main.js
@@ -1,89 +1,117 @@
-import './style.css'
+import "./style.css";
 
 import * as THREE from "three";
-import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
-// import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-// import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-// import { UnrealBloomPass, BloomEffect } from 'three/addons/postprocessing/*';
-// import { OutputPass } from 'three/addons/postprocessing/';
-import { EffectComposer } from 'three/addons/postprocessing/EffectComposer.js';
-import { RenderPass } from 'three/addons/postprocessing/RenderPass.js';
-import { UnrealBloomPass } from 'three/addons/postprocessing/UnrealBloomPass.js';
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls";
+import { EffectComposer } from "three/addons/postprocessing/EffectComposer.js";
+import { RenderPass } from "three/addons/postprocessing/RenderPass.js";
+import { UnrealBloomPass } from "three/addons/postprocessing/UnrealBloomPass.js";
+import { randFloatSpread } from "three/src/math/MathUtils";
 
 const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
-const renderer = new THREE.WebGLRenderer({ canvas: document.querySelector("#bg") });
-renderer.setPixelRatio(window.devicePixelRatio)
+const mainScene = new THREE.Group();
+scene.add(mainScene);
+
+const bloomGroup = new THREE.Group();
+scene.add(bloomGroup);
+
+const camera = new THREE.PerspectiveCamera(
+  75,
+  window.innerWidth / window.innerHeight,
+  0.1,
+  1000
+);
+
+const renderer = new THREE.WebGLRenderer({
+  canvas: document.querySelector("#bg"),
+});
+
+renderer.setPixelRatio(window.devicePixelRatio);
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
-
-
 
 const geometry = new THREE.TorusGeometry(10, 1, 10, 100);
 
 const material = new THREE.MeshStandardMaterial({
-  color: 0xff0000
+  color: 0xff0000,
 });
+
+//material.emissive.setHex(0x000000);
 const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
+mainScene.add(cube);
 
-function generateStars(num,stars, scene) {
+function addStar(scene1, x, y, z) {
+  const color = THREE.MathUtils.randInt(0, 0xffffff)
+  const material = new THREE.MeshBasicMaterial({
+    color: color,
+  });
 
-  for(let count = 0 ; count<stars ; count++){
-    const [x,y,z]= Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(100));
-  
-    const sphere = new THREE.SphereGeometry(0.5, 25, 25);
-    const material = new THREE.MeshStandardMaterial({
-      color: 0xffffff
-    });
-    const star = new THREE.Mesh(sphere,material);
-    star.position.set(x, y, z);
-    scene.add(star);
-  }
+  const starSize= THREE.MathUtils.randInt(4,15);
+  const genericStar = new THREE.SphereGeometry(starSize, 25, 25);
+
+  const star = new THREE.Mesh(genericStar, material);
+
+  star.position.set(x, y, z);
+  scene1.add(star);
 }
 
-
-generateStars(100,250, scene);
 const pointLight = new THREE.PointLight(0xffffff);
-pointLight.position.set(5, 5, 5)
-scene.add(pointLight)
+mainScene.add(pointLight);
 
-const ambientLight = new THREE.AmbientLight(0xffffff, 10000)
-//scene.add(ambientLight)
+camera.position.setZ(50);
 
-const lighthelper = new THREE.PointLightHelper(pointLight)
-const gridHelper = new THREE.GridHelper(200, 50)
-scene.add(lighthelper, gridHelper)
+const renderPass = new RenderPass(scene, camera);
 
-camera.position.setZ(30);
+const bloomPass = new UnrealBloomPass(
+  new THREE.Vector2(window.innerWidth, window.innerHeight),
+  1.5,
+  0.4,
+  0.85
+);
+bloomPass.threshold = 0.05;
+bloomPass.strength = 0.5;
+bloomPass.radius = 0.2;
 
-const renderPass = new RenderPass( scene, camera );
+const composer = new EffectComposer(renderer);
+composer.addPass(renderPass);
+composer.addPass(bloomPass);
 
-const bloomPass = new UnrealBloomPass( new THREE.Vector2( window.innerWidth, window.innerHeight ), 1.5, 0.4, 0.85 );
-bloomPass.threshold = 0;
-bloomPass.strength = 1;
-bloomPass.radius = 0;
+const controls = new OrbitControls(camera, renderer.domElement);
 
-// const outputPass = new OutputPass();
-//const bloomer = new BloomEffect()
+renderer.autoClear = false;
+camera.aspect = window.innerWidth / window.innerHeight;
+camera.updateProjectionMatrix();
 
-const composer = new EffectComposer( renderer );
-composer.addPass( renderPass );
-composer.addPass( bloomPass );
-// composer.addPass( outputPass );
+document.addEventListener("click", onMouseDown, false);
 
-const controls = new OrbitControls(camera, renderer.domElement)
+var raycaster = new THREE.Raycaster();
+var mouse = new THREE.Vector2();
+var plane = new THREE.Plane();
+var planeNormal = new THREE.Vector3();
+var point = new THREE.Vector3();
+
+function onMouseDown(event) {
+  mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
+  mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
+  planeNormal.copy(camera.position).normalize();
+  plane.setFromNormalAndCoplanarPoint(planeNormal, scene.position);
+  raycaster.setFromCamera(mouse, camera);
+  raycaster.ray.intersectPlane(plane, point);
+
+  console.log(point);
+  addStar(bloomGroup, point.x, point.y, point.z);
+}
 
 function animate() {
-  rotation()
   requestAnimationFrame(animate);
-  renderer.render(scene, camera);
-  controls.update()
+
+  controls.update();
   composer.render();
+
+  //bloomPass.radius += bloomPass.radius < 1? 0.008 : -0.5; 
+
+  bloomPass.strength += bloomPass.strength < 1? 0.005 : -0.5;
+  renderer.render(scene, camera);
 }
-function rotation() {
-  cube.rotation.x += 0.004;
-  cube.rotation.y += 0.01;
-}
+
 animate();
